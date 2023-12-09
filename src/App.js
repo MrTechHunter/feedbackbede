0b6cb50bed5494b04306a48175026b2fb6b144da
Mahdi Hamldar <mahdihamldar@gmail.com>
2023-12-09 11:08:39 +0330

    Files added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import React, { Component } from "react";
+
+import logo from "./logo.svg";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
